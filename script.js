abe54e3ca6809e5a5cf2cacd59bf98332472aec0
Dariusz Podczasik <boostertech@mail.com>
2023-06-24 20:29:12 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+{
+    let rockButton = document.querySelector(".js-rockButton");
+    let paperButton = document.querySelector(".js-paperButton");
+    let scissorButton = document.querySelector(".js-scissorButton");
+
+    let resultElement = document.querySelector(".js-resultElement");
+
+    const printResult = (userPickParameter, computerPickParameter, resultParameter) => {
+        resultElement.innerHTML =
+            `You picked: ${userPickParameter}<br>
+             Computer picked: ${computerPickParameter}<br><br>
+             ${resultParameter.toUpperCase()}`;
+    }
+
+    const generateComputerChoice = () => {
+        const randomNumber = Math.random();
+        let computerChoice = "";
+
+        if (randomNumber >= 0 && randomNumber <= 0.33) computerChoice = "rock"
+        else if (randomNumber > 0.33 && randomNumber <= 0.66) computerChoice = "paper";
+        else if (randomNumber > 0.66 && randomNumber <= 1) computerChoice = "scissor";
+
+        return computerChoice;
+    }
+
+    const playGame = (userChoiceParameter) => {
+        let resultText = "";
+        const computerChoice = generateComputerChoice();
+
+        if (userChoiceParameter === computerChoice) resultText = "Draw! Try again"
+        else {
+            if (userChoiceParameter === "rock") (computerChoice === "paper") ? resultText = "You lost!" : resultText = "You win!";
+            if (userChoiceParameter === "paper") (computerChoice === "rock") ? resultText = "You win!" : resultText = "You lost!";
+            if (userChoiceParameter === "scissor") (computerChoice === "paper") ? resultText = "You win!" : resultText = "You lost!";
+        }
+
+        printResult(userChoiceParameter, computerChoice, resultText);
+    }
+
+    const userButtonClick = () => {
+
+        rockButton.addEventListener("click", () => { playGame("rock") });
+        paperButton.addEventListener("click", () => { playGame("paper") });
+        scissorButton.addEventListener("click", () => { playGame("scissor") });
+    }
+
+    const init = () => {
+        userButtonClick();
+    }
+
+    init();
+}
